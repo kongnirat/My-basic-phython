61f44aa2b79da1d195583c24b64e275df9e7c5cc
kongnirat <2549kong@gmail.com>
2024-04-19 23:22:21 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+x=20
+#แปลงเป็นstringโดยใช้str
+print('ผลลัพธ์ = '+str(x))
+
+#ตัวเช็ตข้อมูล
+print(type(x))
+
+#การแปลงจาก str เป็น int,foat
+a = 12
+y = "45"
+
+result = a+int(y)#สามารภเปลี่ยนเป็น foatได้โดยนำไปวางไว้แทน int
+print(result)
+
+#function input
+print('hello bro what your name?')
+firstname = input ('your name: ')#รับค่าที่ user พิมพ์ลงตัวแปรfname
+lastname =input('your last name: ')
+print('hello' ,firstname + lastname)
+
