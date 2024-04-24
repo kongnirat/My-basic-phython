8ad70b3bfd09a624af8e622e11f37efd7432ab85
kongnirat <2549kong@gmail.com>
2024-04-24 14:13:45 +0700

    Create basic-part2.py

---
+++ basic-part2.py
@@ -0,0 +1,13 @@
+#Part2
+#โครงสร้างควบคุม (Control Structure)_แบบเลือก
+#โครงสร้าง if boolean Exception:
+    #Statement
+    
+age = int(input("พิมพ์มอายุของคุณ: "))
+name = str(input("ชื่อของคุณ: "))
+
+if age>=15:
+    print("นาย/นางสาว" + name)
+    print("จบการทำงานif")
+
+print("end")
