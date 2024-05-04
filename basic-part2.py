dd5c7648c0104e0d32294f4500975ab9abd48164
kongnirat <2549kong@gmail.com>
2024-05-04 13:08:36 +0700

    Update basic-part2.py

--- basic-part2.py
+++ basic-part2.py
@@ -9,55 +9,12 @@ age = int(input("พิมพ์มอายุของคุณ: "))
 name = str(input("ชื่อของคุณ: "))
 gender = str(input("เพศของคุณ: "))
 
-if age>=15 or age<=24 and gender==("ชาย") :#ระบุเงื่อนไขที่เป็นจริงจะทำงานในคำสั่ง if
-    print("นาย/นางสาว" + name)
-    print("วัยรุ่น")
-    print("เพศชาย")   
-    
-elif age>=15 or age<=24 and gender==("หญิง"):
-    print("นาย/นางสาว" + name)
-    print("วัยรุ่น")
-    print("เพศหญิง")
 
-elif age>=15 or age<=24 and gender==("LGBTQ+"): 
-    print("นาย/นางสาว" + name)
-    print("วัยรุ่น")
-    print("LGBTQ+")
-     
-elif age>=25 or age<=55 and gender==("ชาย"):#เป็นเงื่อนไขที่รองลงมาจากifเพื่อบังคับทำงานเคสเดียว
-    print("นาย/นางสาว" + name)
-    print("วัยทำงาน")
-    print("เพศชาย")
-    
-elif age>=25 or age<=55 and gender==("หญิง"):
-    print("นาย/นางสาว" + name)
-    print("วัยทำงาน")
-    print("เพศหญิง")
-    
-elif age>=25 or age<=55 and gender==("LGBTQ+"):
-    print("นาย/นางสาว" + name)
-    print("วัยทำงาน")
-    print("LGBTQ+")
-     
-elif age>=56 or age<=70  and gender==("ชาย"):
-    print("นาย/นางสาว" + name)
-    print("วัยชรา")
-    print("เพศชาย")
-    
-elif age>=56 or age<=70 and gender==("หญิง"):
-    print("นาย/นางสาว" + name)
-    print("วัยชรา")
-    print("เพศหญิง")
-
-elif age>=56 or age<=70 and gender==("LGBTQ+"):
-    print("นาย/นางสาว" + name)
-    print("วัยชรา")
-    print("LGBTQ+")
-      
-else :
-    print("เด็กชาย/เด็กหญิง " + name)
-    print("วัยเด็ก")
-    print("เพศ",gender)
-
-print("end")
-
+#if ซ้อน if
+if age>=15 :   
+    if age<=20:
+            print("นาย/นางสาว" + name)
+            print("วัยรุ่น")
+            print("เพศชาย")   
+else : 
+    print("noting")
